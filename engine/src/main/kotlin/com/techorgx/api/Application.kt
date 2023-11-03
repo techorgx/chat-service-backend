168956de4ef8b870d381a89cf7ddcc49524aa0d0
Aman Rajput <amrajput@expediagroup.com>
2023-11-03 18:32:44 +0530

    Setup ktor dependencies for websocket

---
+++ engine/src/main/kotlin/com/techorgx/api/Application.kt
@@ -0,0 +1,38 @@
+package com.techorgx.api
+
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.websocket.*
+import io.ktor.websocket.*
+import java.time.Duration
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+fun Application.module() {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+    routing {
+        get("/") {
+            call.respondText("Hello, world!")
+        }
+
+        webSocket("/echo") {
+            send("Please enter your name")
+            for (frame in incoming) {
+                frame as? Frame.Text ?: continue
+                val receivedText = frame.readText()
+                if (receivedText.equals("bye", ignoreCase = true)) {
+                    close(CloseReason(CloseReason.Codes.NORMAL, "Client said BYE"))
+                } else {
+                    send(Frame.Text("Hi, $receivedText!"))
+                }
+            }
+        }
+    }
+}
